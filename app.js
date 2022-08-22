d4e31e02a6fd1145b6d923ebbe70694e63e83308
Ryan J. Parker <ryan.j.parker@wsu.edu>
2022-08-22 14:05:42 -0700

    add new render function with embedded event listeners as alternative to dropdown

--- app.js
+++ app.js
@@ -49,7 +49,6 @@ async function displayFilteredPosts(value) {
     const filteredArray = await getPostsByCategory(value);
     const filteredPosts = renderPosts(filteredArray);
     filteredArray.length === 0 ? bulletinBoard.append(pTag) : bulletinBoard.append(filteredPosts);
-
 }
 
 /*
@@ -77,3 +76,73 @@ async function displayFilteredPosts(value) {
     
 back to index.html
 */
+
+function renderCatsOnTheFly() {
+    
+    const catDiv = document.createElement('div');
+    const catOne = document.createElement('button');
+    catOne.textContent = 'For sale';
+    catOne.classList.add('cat-btn');
+
+    const catTwo = document.createElement('button');
+    catTwo.textContent = 'Free';
+    catTwo.classList.add('cat-btn');
+    
+    const catThree = document.createElement('button');
+    catThree.textContent = 'Event';
+    catThree.classList.add('cat-btn');
+    
+    const catFour = document.createElement('button');
+    catFour.textContent = 'Housing';
+    catFour.classList.add('cat-btn');
+    
+    const catFive = document.createElement('button');
+    catFive.textContent = 'Misc';
+    catFive.classList.add('cat-btn');
+
+    const noMatch = document.createElement('p');
+    noMatch.textContent = 'No posts match your request';
+    noMatch.classList.add('no-match');
+    
+    catOne.addEventListener('click', async () => {
+        bulletinBoard.textContent = '';
+        const forSalePosts = await getPostsByCategory(1);
+        const forSaleP = renderPosts(forSalePosts);
+        forSalePosts.length === 0 ? bulletinBoard.append(noMatch) : bulletinBoard.append(forSaleP);
+    });
+
+    catTwo.addEventListener('click', async () => {
+        bulletinBoard.textContent = '';
+        const freePosts = await getPostsByCategory(2);
+        const freeP = renderPosts(freePosts);
+        freePosts.length === 0 ? bulletinBoard.append('No posts match your request') : bulletinBoard.append(freeP);
+    });
+
+    catThree.addEventListener('click', async () => {
+        bulletinBoard.textContent = '';
+        const eventPosts = await getPostsByCategory(3);
+        const eventP = renderPosts(eventPosts);
+        eventPosts.length === 0 ? bulletinBoard.append('No posts match your request') : bulletinBoard.append(eventP);
+    });
+
+    catFour.addEventListener('click', async () => {
+        bulletinBoard.textContent = '';
+        const housingPosts = await getPostsByCategory(4);
+        const housingP = renderPosts(housingPosts);
+        housingPosts.length === 0 ? bulletinBoard.append('No posts match your request') : bulletinBoard.append(housingP);
+    });
+
+    catFive.addEventListener('click', async () => {
+        bulletinBoard.textContent = '';
+        const miscPosts = await getPostsByCategory(5);
+        const miscP = renderPosts(miscPosts);
+        miscPosts.length === 0 ? bulletinBoard.append('No posts match your request') : bulletinBoard.append(miscP);
+    });
+
+    catDiv.classList.add('cat-div');
+    catDiv.append(catOne, catTwo, catThree, catFour, catFive);
+    bulletinBoard.append(catDiv);
+
+}
+
+renderCatsOnTheFly();
